f8a297ed3bae311c1506324875145823f4ebf2e9
Robert_Enc <140596327+BobertInc@users.noreply.github.com>
2026-06-17 13:36:42 -0500

    oled

--- src/oled.cpp
+++ src/oled.cpp
@@ -6,24 +6,20 @@
 #include "main.h"
 #include "encoder.h"
 
-
-// OLED configuration
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET    -1 // Sharing ESP32 reset pin
-#define OLED_ADDR   0x3C // Standard 0.96" OLED I2C address
+#define OLED_RESET    -1 
+#define OLED_ADDR   0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &PendulumI2C, OLED_RESET);
 
 unsigned long lastOledTime = 0;
-const unsigned long oledRefreshInterval = 150; // Update display every 150ms to save loop speed
+const unsigned long oledRefreshInterval = 150;
 
 void setup_oled(){
-      // Initialize the OLED screen
   if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     Serial.println(F("OLED allocation failed! Check your wiring."));
-    // Don't freeze completely, keep going if you just want to check hardware logs
   } else {
     display.clearDisplay();
     display.setTextColor(SSD1306_WHITE);
@@ -36,8 +32,6 @@ void setup_oled(){
 }
 
 void loop_oled(){
-    // --- NON-BLOCKING OLED UPDATE SEGMENT ---
-  // Only write to the screen if enough time has passed to safeguard loop speed
   if (millis() - lastOledTime >= oledRefreshInterval) {
     lastOledTime = millis();
     
@@ -48,7 +42,7 @@ void loop_oled(){
     display.setTextSize(2);
     display.setCursor(0, 0);
     //display.print("Cart Pos: ");
-    display.print(gCartPosition, 1); // Prints current angle with 1 decimal accuracy
+    display.print(gCartPosition, 1);
     display.setCursor(0, 21);
     //display.print("Th: ");
     display.print(gPend, 2);
@@ -56,24 +50,10 @@ void loop_oled(){
     //display.print("ThS: ");
     display.print(gPendSpeed, 2);
     /*
-    
-    // Header Label
     display.setTextSize(1);
     display.setCursor(0, 0);
     display.print("PENDULUM ANGLE:");
-    
-    // Large Dynamic Reading Frame
-    display.setTextSize(3);
-    display.setCursor(10, 24);
-    display.print(theta, 1); // Prints current angle with 1 decimal accuracy
-    display.print((char)247); // Adds degree symbol (°)
-    
-    display.setTextSize(1);
-    display.setCursor(0, 56);
-    display.print("JAJA XD PE CAUSE BE CAUSE:");
-    */
-
-    // Push buffer data to screen hardware
     display.display();
+    */
   }
 }
